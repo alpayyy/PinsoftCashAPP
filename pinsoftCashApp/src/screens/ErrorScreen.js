b306e2c4bb6a8842414407c7e5b206a170252719
alpayyy <115874416+alpayyy@users.noreply.github.com>
2023-09-12 10:29:22 +0300

    Update ErrorScreen.js

--- pinsoftCashApp/src/screens/ErrorScreen.js
+++ pinsoftCashApp/src/screens/ErrorScreen.js
@@ -1,15 +1,5 @@
-// ErrorScreen.js
-
 import React from "react";
-import { View, Text, Button } from "react-native";
-
-const ErrorScreen = ({ navigation }) => {
-  const handleRetry = () => {
-    // Tekrar Dene düğmesine basıldığında yapılacak işlemleri burada gerçekleştirin
-    // ErrorScreen.js
-
-import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const ErrorScreen = ({ navigation }) => {
   const handleRetry = () => {
@@ -18,37 +8,29 @@ const ErrorScreen = ({ navigation }) => {
   };
 
   const handleGoBack = () => {
-    // AnaMenü Dön düğmesine basıldığında yapılacak işlemleri burada gerçekleştirelim
-    
-    navigation.navigate("HomePage"); 
-  };
-
-  return (
-    <View>
-      <Text>İşlem Başarısız!</Text>
-      <Button title="Tekrar Dene" onPress={handleRetry} />
-      <Button title="AnaMenüye Dön" onPress={handleGoBack} />
-    </View>
-  );
-};
-
-export default ErrorScreen;
-
-  };
-
-  const handleGoBack = () => {
     // AnaMenü Dön düğmesine basıldığında yapılacak işlemleri burada gerçekleştirin
-    
     navigation.navigate("HomePage"); 
   };
 
   return (
-    <View>
-      <Text>İşlem Başarısız!</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>İşlem Başarısız!</Text>
       <Button title="Tekrar Dene" onPress={handleRetry} />
-      <Button title="AnaMenüye Dön" onPress={handleGoBack} />
+      <Button title="Ana Menüye Dön" onPress={handleGoBack} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
 
 export default ErrorScreen;
